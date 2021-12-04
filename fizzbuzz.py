7357e60ad41a9bc0e2cb4224190029a44d853c37
togashitoga <wqfqw3200@kjb.biglobe.ne.jp>
2021-12-04 13:18:52 +0900

    3の倍数だけ

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,4 @@
 def fizzbuzz_convert(number):
-    if number % 15 == 0:
-        return 'FizzBuzz'
     if number % 3 == 0:
         return 'Fizz'
-
-    if number % 5 == 0:
-        return 'Buzz'
-
-    else:
-        return number
-
-
 assert fizzbuzz_convert(3) == 'Fizz'
